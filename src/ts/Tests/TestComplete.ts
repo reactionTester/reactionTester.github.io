ff36cfd847481539779ff15d9712df3335773abc
Abecassis Thomas <stingerkiller34@gmail.com>
2024-09-02 11:37:16 +0200

    fix completeTest

--- src/ts/Tests/TestComplete.ts
+++ src/ts/Tests/TestComplete.ts
@@ -2,6 +2,7 @@ import TestAudio from './TestAudio.js';
 import TestVisual from './TestVisual.js';
 import TestTouch from './TestTouch.js';
 import Test from './Test.js';
+import { State } from '../State.enum.js';
 
 export default class TestComplete extends Test {
     private testAudio: TestAudio;
@@ -11,6 +12,9 @@ export default class TestComplete extends Test {
 
     constructor(blueColorHex: string, greenColorHex: string) {
         super();
+        this.buttonTitleIdle = 'Complete Reaction Time Test.';
+        this.Message1TitleIdle = 'A random test will start when you click the button, click when you hear a beep, feel a vibration or the button turns green.';
+        this.buttonTitleWaitingBeep = 'Click when you hear a beep, feel a vibration or the button turns green.';
         this.testAudio = new TestAudio();
         this.testVisual = new TestVisual(blueColorHex, greenColorHex);
         this.testTouch = new TestTouch();
@@ -18,28 +22,20 @@ export default class TestComplete extends Test {
     }
 
     startAction(): void {
-        this.currentTest = this.runRandomTest();
+        this.currentTest = this.getRandomTest();
+        this.currentTest.startAction();
     }
 
-    /*
-    * @function runRandomTest
-    * @description Run a random test and return it
-    * @returns {Test} the test that was run
-    */
-    private runRandomTest(): Test {
-        let randomTest = Math.floor(Math.random() * 3);
-        switch (randomTest) {
+    private getRandomTest(): Test {
+        let randomNumber: number = Math.floor(Math.random() * 3);
+        switch (randomNumber) {
             case 0:
-                this.currentTest = this.testAudio;
-                break;
+                return this.testAudio;
             case 1:
-                this.currentTest = this.testVisual;
-                break;
-            case 2:
-                this.currentTest = this.testTouch;
-                break;
+                return this.testVisual;
+            default:
+                return this.testTouch;
         }
-        return this.currentTest;
     }
 
     stopAction(): void {
@@ -47,6 +43,7 @@ export default class TestComplete extends Test {
     }
 
     initializeDOMElements(): void {
+        super.initializeDOMElements();
         this.testAudio.initializeDOMElements();
         this.testVisual.initializeDOMElements();
         this.testTouch.initializeDOMElements();
